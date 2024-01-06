825774a80afab30ad91bf701603302d870d17bac
Limphus12 <Limphus12@gmail.com>
2024-01-06 16:52:28 +0000

    ui stuff

--- Project/Assets/Scripts/UIManager.cs
+++ Project/Assets/Scripts/UIManager.cs
@@ -8,9 +8,12 @@ namespace com.limphus.extraction_shooter
 {
     public class UIManager : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI interactionText;
+        [SerializeField] private TextMeshProUGUI interactionText, ammoText;
+        [SerializeField] private GameObject crosshairUI;
 
         private InteractionScript interaction;
+
+        private Firearm firearm;
 
         private void Awake()
         {
@@ -21,9 +24,23 @@ namespace com.limphus.extraction_shooter
         {
             //grab references
             interaction = GameManager.Player.GetComponent<InteractionScript>();
+            firearm = GameManager.Player.GetComponentInChildren<Firearm>();
 
             //sub to events
             interaction.OnInteractionCheck += Interaction_OnInteractionCheck;
+            firearm.OnAmmoChanged += Firearm_OnAmmoChanged;
+            firearm.OnAim += Firearm_OnAim;
+
+        }
+
+        private void Firearm_OnAim(object sender, utilities.Events.OnBoolChangedEventArgs e)
+        {
+            if (crosshairUI) crosshairUI.SetActive(!e.i);
+        }
+
+        private void Firearm_OnAmmoChanged(object sender, utilities.Events.OnIntChangedEventArgs e)
+        {
+            if (ammoText) ammoText.text = e.i.ToString();
         }
 
         private void Interaction_OnInteractionCheck(object sender, InteractionScript.OnInteractEventArgs e)
